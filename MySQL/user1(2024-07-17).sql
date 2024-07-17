78df3de2964437e2ef862940517fd12cb6c7adc4
jinh0318a <jinh0318a@naver.com>
2024-07-17 13:37:00 +0900

    user1

---
+++ MySQL/user1(2024-07-17).sql
@@ -0,0 +1,11 @@
+-- 여기는 USER1 으로 접속한 화면
+use mydb;
+select * from tbl_student;
+
+-- S00001, 홍길동, 정보통신, 3, 010-1111-1111, 서울특별시
+desc tbl_student;
+insert into tbl_student(st_num, st_name, st_dept, st_grade, st_tel, st_addr) 
+values('S00001','홍길동','정보통신','3','010-1111-1111','서울특별시');
+
+-- 학번이 S00001 인 학생의 주소를 강원도 원주로 변경
+update tbl_student set st_addr='강원도 원주' where st_num='S00001';
